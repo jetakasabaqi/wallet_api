1c40a0561ee463a9361ee206f0b8d75f2393653d
Vesa B <vesabaruti@gmail.com>
2019-10-21 16:59:22 +0200

    user service

---
+++ src/main/java/abc/workshop/wallet/service/UserService.java
@@ -0,0 +1,32 @@
+package abc.workshop.wallet.service;
+
+import abc.workshop.wallet.model.User;
+import abc.workshop.wallet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id){
+        userRepository.deleteById(id);
+    }
+
+}
