999cc5805f4d2136b0a4839e23fba502367778dc
Vesa B <vesabaruti@gmail.com>
2019-10-23 12:48:51 +0200

    add repository annotation

--- src/main/java/abc/workshop/wallet/repository/UserRepository.java
+++ src/main/java/abc/workshop/wallet/repository/UserRepository.java
@@ -2,6 +2,8 @@ package abc.workshop.wallet.repository;
 
 import abc.workshop.wallet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
